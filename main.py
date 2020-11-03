f0992e425adfb3a0fb664cb72e0b470dc43bf4e9
Arthur <arthur.costil@epitech.eu>
2020-11-04 00:25:39 +0100

    Adding beginning of usage with get_opt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import feedparser
+import feedparser, sys, getopt
 from termcolor import colored
 
 class Feed_rss:
@@ -20,13 +20,43 @@ class Feed_rss:
             x += 1
         return (0)
 
+    def print_usage():
+        print("0.0.2")
+        return(0)
+
+    def print_command():
+        print("Commande")
+        return (0)
+    
+    def add_feed():
+        print("Add feed")
+        return (0)
+
 if __name__ == "__main__":
-    with open("./urls.txt") as file:
-        while 1:
-            data = file.readline()
-            if (data == ""):
-                exit(0)
-            else :
-                url = Feed_rss.set_rss(data)
-                compt = Feed_rss.get_all_entries(url)
-                entry = Feed_rss.get_rss(url, compt)
+    arg = sys.argv[1:]
+    options = "hau"
+    long_options = ["Help", "Add", "Usage", "usage", "add", "help"]
+    try:
+        arguments, values = getopt.getopt(arg, options, long_options)
+        for currentArgument, currentValue in arguments:
+
+            if currentArgument in ("-h", "-help", "-Help", "--h","--help", "--Help"):
+                Feed_rss.print_command()
+            
+            if currentArgument in ("-u", "-usage", "-Usage","--u", "--usage", "--Usage"):
+                Feed_rss.print_usage()
+
+            if currentArgument in ("-a", "-add", "-Add", "--a", "--add", "--Add"):
+                Feed_rss.add_feed()
+    except getopt.error as err:
+        # output error, and return with an error code
+        print (str(err))
+    # with open("./urls.txt") as file:
+    #     while 1:
+    #         data = file.readline()
+    #         if (data == ""):
+    #             exit(0)
+    #         else :
+    #             url = Feed_rss.set_rss(data)
+    #             compt = Feed_rss.get_all_entries(url)
+    #             entry = Feed_rss.get_rss(url, compt)
