1e7469929c23b965f87231370a8ec5d2bbfd9e29
Arthur <arthur.costil@epitech.eu>
2020-11-05 21:30:47 +0100

    Maybe using self and create the object is a good idea

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
 import feedparser, sys, getopt
 from termcolor import colored
 
-class Feed_rss:
-    def set_rss(url):
+class Feed_rss():
+
+    def set_rss(self, url):
         feed = feedparser.parse(url)
         return (feed)
 
-    def get_all_entries(feed):
+    def get_all_entries(self, feed):
         all_entries = len(feed.entries)
         return(all_entries)
 
-    def get_rss(feed, compt):
-        print ('Number of RSS posts :', compt, '\n')
+    def get_rss(self, feed, compt):
+        print('Number of RSS posts :', compt, '\n')
         x = 0
         while (x < compt):
             entry = feed.entries[x]
-            print (colored('Title :', 'green'), entry.title, '\n\n',
+            print(colored('Title :', 'green'), entry.title, '\n\n',
             colored(entry.link, 'blue'), '\n')
             x += 1
         return (0)
 
-    def print_usage():
+    def print_usage(self):
         print("0.0.2")
         return(0)
 
-    def print_command():
+    def print_command(self):
         print("Commande")
         return (0)
     
-    def add_feed():
+    def add_feed(self):
         print("Add feed")
         return (0)
 
-    def get_flag(arg):
+    def get_flag(self, arg):
         options = "hau"
         long_options = ["Help", "Add", "Usage", "usage", "add", "help"]
         try:
@@ -49,14 +50,15 @@ class Feed_rss:
                     Feed_rss.add_feed()
 
         except (getopt.error) as err:
-            print (str(err))
+            print(str(err))
             return (9)
         return (0)
 
 
 if __name__ == "__main__":
     arg = sys.argv[1:]
-    x = Feed_rss.get_flag(arg)
+    Feed = Feed_rss()
+    x = Feed.get_flag(arg)
     if (x == 0):
         with open("./urls.txt") as file:
             while 1:
@@ -64,6 +66,6 @@ if __name__ == "__main__":
                 if (data == ""):
                     exit(0)
                 else :
-                    url = Feed_rss.set_rss(data)
-                    compt = Feed_rss.get_all_entries(url)
-                    entry = Feed_rss.get_rss(url, compt)
+                    url = Feed.set_rss(data)
+                    compt = Feed.get_all_entries(url)
+                    entry = Feed.get_rss(url, compt)
